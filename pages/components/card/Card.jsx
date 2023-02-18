ea64deec1f26b31666b24473baf26266fe0c1ddb
tanmoypaul1005 <tanmoypaul1005@gmail.com>
2023-02-18 21:13:20 +0600

    ok

--- pages/components/card/Card.jsx
+++ pages/components/card/Card.jsx
@@ -6,8 +6,7 @@ const Card = ({ title,description,image }) => {
         <div>
             <div className="flex justify-center">
                 <div className="flex flex-col md:flex-row md:max-w-xl rounded-lg bg-white shadow-lg">
-                    <img className=" w-full h-96 md:h-auto object-cover md:w-48 rounded-t-lg md:rounded-none md:rounded-l-lg" 
-                    src={image} alt="" />
+    
                     <div className="p-6 flex flex-col justify-start">
                         <h5 className="text-gray-900 text-xl font-medium mb-2">{title}</h5>
                         <p className="text-gray-700 text-base mb-4">
